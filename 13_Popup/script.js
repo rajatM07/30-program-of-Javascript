7502d7f9345cba92b102e551a39a4c548183c28e
rajat mehra <rajatmehra017@gmail.com>
2024-11-02 00:32:32 +0530

    feat: Popup

---
+++ 13_Popup/script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function showPopup() {
+  popup.classList.add("open-popup");
+}
+
+function hidePopup() {
+  popup.classList.remove("open-popup");
+}
